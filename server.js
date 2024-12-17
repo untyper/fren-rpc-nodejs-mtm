765b7ae4ae0769b99c7ff08b8fa203d687788c4c
untyper <76541296+untyper@users.noreply.github.com>
2024-12-17 22:34:14 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,70 @@
+// Copyright (c) [2024] [Jovan J. E. Odassius]
+//
+// License: MIT (See the LICENSE file in the root directory)
+// Github: https://github.com/untyper/<INCOMING>
+
 import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
+import https from 'https';
+import tls from 'tls';
+// import fs from "fs";
+import { readFile } from 'fs/promises';
+import { URL } from 'url';
+
+// This servers port
+const PORT = 443; // HTTPS
+
+async function loadConfig() {
+  return JSON.parse(await readFile(new URL('./config.json', import.meta.url), 'utf8'));
+}
+
+// Function to load SSL certificates
+async function loadCertificates() {
+  let key, cert;
+
+  try {
+    key = await readFile('/etc/letsencrypt/live/yourdomain.com/privkey.pem');
+    cert = await readFile('/etc/letsencrypt/live/yourdomain.com/fullchain.pem');
+  } catch (err) {
+    console.error(`Something went wrong trying to load Let's Encrypt SSL certificates.`, err);
+    return;
+  }
+
+  return { key, cert };
+}
+
+const config = await loadConfig();
 
 const app = express();
-const rpcUrl = 'http://127.0.0.1:8332';
+const rpcUrl = `http://127.0.0.1:${config.rpcPort}`;
 const rpcAuth = {
-  username: 'username',
-  password: 'password',
+  username: config.rpcUsername,
+  password: config.rpcPassword,
 };
+
+// Load certificates and create a secure context
+let sslOptions = await loadCertificates();
+let secureContext =  tls.createSecureContext(sslOptions);
+  
+// Periodically reload certificates and update the secure context
+setInterval(async () => {
+  try {
+    sslOptions = await loadCertificates();
+    secureContext = tls.createSecureContext(sslOptions);
+    console.log('Secure context updated successfully.');
+  } catch (err) {
+    console.error('Error updating SSL certificates:', err);
+  }
+}, 60 * 60 * 1000); // Reload every hour
+  
+// HTTPS server with SNICallback to use the updated secure context
+const server = https.createServer({
+  SNICallback: (serverName, callback) => {
+  callback(null, secureContext);
+  },
+  ...sslOptions, // Initial certificates
+}, app);
 
 // Enable CORS
 app.use(cors({
@@ -31,7 +88,7 @@ app.use(express.json());
 
 // RPC Proxy Endpoint
 app.post('/', async (req, res) => {
-    console.log(req.body)
+  console.log(req.body)
   try {
     const response = await axios.post(rpcUrl, req.body, {
       auth: rpcAuth,
@@ -43,8 +100,7 @@ app.post('/', async (req, res) => {
   }
 });
 
-// Start the server
-const PORT = 8334;
-app.listen(PORT, () => {
+// Start the HTTPS server
+server.listen(PORT, () => {
   console.log(`Node.js RPC server running on https://localhost:${PORT}`);
 });
